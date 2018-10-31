943b58551cf0bb36621bc05f153c3d31c6817752
garrett <gvhagopian@gmail.com>
2018-10-30 17:34:13 -0700

    added pages, added sequelize

--- ingrid/routes/index.js
+++ ingrid/routes/index.js
@@ -1,10 +1,24 @@
-﻿'use strict';
-var express = require('express');
-var router = express.Router();
+﻿'use strict'
+var express = require('express')
+var router = express.Router()
+
 
 /* GET home page. */
-router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
-});
+router.get('/', (req, res) => {
+    res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+router.get('/Page1', (req, res) => {
+    res.render('Page1', { title: 'Page 1' })
+})
+
+
+router.post('/Page1', (req, res) => {
+    res.render('Page1', {answer: req.body.srvResp})
+})
+
+router.get('/Page2', (req, res) => {
+    res.render('Page2', { title: 'Page 2' })
+})
+
+module.exports['default'] = router
