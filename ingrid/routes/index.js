9b67e511a1a6a26d0fe5695626ccde3b37becd83
garrett <gvhagopian@gmail.com>
2018-12-04 13:42:25 -0800

    fixed some issues with links, added images

--- ingrid/routes/index.js
+++ ingrid/routes/index.js
@@ -18,7 +18,26 @@ router.get('/1', (req, res) => {
         })
         .spread((surveyRespondent, created) => {
             req.session.respID = surveyRespondent.id
-            res.render('survey-form', { buttons: ["Submit Publicly", "Submit Privately"] })
+            res.render('survey-form', { title: "Page 1", buttons: ["Submit Publicly", "Submit Privately"] })
+        })
+        .catch(error => {
+            res.status(400).send(error)
+        })
+})
+
+
+router.get('/2', (req, res) => {
+    model.surveyRespondent
+        .findOrCreate({
+            where: {
+                userAgent: req.headers['user-agent'],
+                ipAddress: req.connection.remoteAddress,
+                cookie: req.sessionID
+            }
+        })
+        .spread((surveyRespondent, created) => {
+            req.session.respID = surveyRespondent.id
+            res.render('survey-form', { title: "Page 2", buttons: ["Submit"] })
         })
         .catch(error => {
             res.status(400).send(error)
@@ -36,30 +55,16 @@ router.post('/1', (req, res) => {
             submitButton: req.body.button
         })
         .then(surveyResponse => {
-            res.render('survey-form', { buttons: ["Submit Publicly", "Submit Privately"], comment: req.body.comment, identifier: surveyResponse.identifier })
+            res.render('survey-form',
+                {
+                    title: "Page 1",
+                    buttons: ["Submit Publicly", "Submit Privately"],
+                    comment: { text: req.body.comment, button: req.body.button },
+                    identifier: surveyResponse.identifier
+                })
         })
         .catch(error => res.status(400).send(error))
 })
-
-
-router.get('/2', (req, res) => {
-    model.surveyRespondent
-        .findOrCreate({
-            where: {
-                userAgent: req.headers['user-agent'],
-                ipAddress: req.connection.remoteAddress,
-                cookie: req.sessionID
-            }
-        })
-        .spread((surveyRespondent, created) => {
-            req.session.respID = surveyRespondent.id
-            res.render('survey-form', { buttons: ["Submit"] })
-        })
-        .catch(error => {
-            res.status(400).send(error)
-        })
-})
-
 
 router.post('/2', (req, res) => {
     model.surveyResponse
@@ -71,7 +76,13 @@ router.post('/2', (req, res) => {
             submitButton: req.body.button
         })
         .then(surveyResponse => {
-            res.render('survey-form', { buttons: ["Submit"], comment: req.body.comment, identifier: surveyResponse.identifier })
+            res.render('survey-form',
+                {
+                    title: "Page 2",
+                    buttons: ["Submit"],
+                    comment: { text: req.body.comment, button: req.body.button },
+                    identifier: surveyResponse.identifier
+                })
         })
         .catch(error => res.status(400).send(error))
 })
