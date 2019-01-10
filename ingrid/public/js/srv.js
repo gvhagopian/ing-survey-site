3cdcf4012939d6495fe69372595d9822d0965238
garrett <gvhagopian@gmail.com>
2019-01-10 00:23:06 -0800

    Another revision - this time client requested that it look somewhat reminiscent of a certain website.

--- ingrid/public/js/srv.js
+++ ingrid/public/js/srv.js
@@ -33,19 +33,43 @@
     window.addEventListener('load', function () {
         var forms = document.getElementsByClassName('needs-validation');
 
+        var inputList = [{ t: new Date().getTime(), v: "pageload" }];
+        document.getElementById("inputComment").addEventListener("focus", function (evt) {
+            inputList.push({ t: new Date().getTime(), v: "gotfocus" });
+        });
+        document.getElementById("inputComment").addEventListener("blur", function (evt) {
+            inputList.push({ t: new Date().getTime(), v: "lostfocus" });
+        });
+        document.getElementById("inputComment").addEventListener("keydown", function (evt) {
+            var input = { t: new Date().getTime() };
+            if (evt.char) {
+                input.v = evt.char;
+            } else if (evt.key && evt.key !== "Unidentified") {
+                input.v = evt.key;
+            } else {
+                input.v = evt.code;
+            }
+            inputList.push(input);
+        });
+
         var validation = Array.prototype.filter.call(forms, function (form) {
             form.addEventListener('submit', function (event) {
-                if (form.checkValidity() === false) {
+                var isValid = form.checkValidity();
+                inputList.push({ t: new Date().getTime(), v: "submit" + (isValid ? "valid" : "invalid") });
+                if (isValid === false) {
                     event.preventDefault();
                     event.stopPropagation();
                     var errorElements = document.querySelectorAll("input.form-control:invalid");
                     scrollTo(errorElements[0].getBoundingClientRect().top, 500);
                     //scrollTo(document.getElementsByTagName('html'), errorElements[0].getBoundingClientRect().top, 2000);
                 }
+                document.getElementById("inputHistory").value = JSONC.pack(inputList);
+                console.log(document.getElementById("inputHistory").value);
+                window.unpacked = JSONC.unpack(document.getElementById("inputHistory").value);
                 form.classList.add('was-validated');
             }, false);
         });
-        if (getComputedStyle(document.body).backgroundColor !== "rgb(255, 255, 255)") {
+        if (getComputedStyle(document.body).backgroundColor !== "rgb(232, 235, 238)") {
             var obfs = document.getElementsByClassName('obfuscated-black');
 
             Array.prototype.filter.call(obfs, function (obf) {
